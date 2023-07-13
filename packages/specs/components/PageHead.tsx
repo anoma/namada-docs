e79e83c286a1da946a6f97225ecb28816d09636c
Pedro Rezende <xpedrorezende@gmail.com>
2023-07-13 14:56:04 +0200

    feat(specs): updating opengraph image and head structure

---
+++ packages/specs/components/PageHead.tsx
@@ -0,0 +1,15 @@
+export const PageHead = () => {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        property="og:image"
+        content="https://specs.namada.net/opengraph.jpg"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@namada" />
+      <meta name="twitter:creator" content="@namada" />
+      <meta property="og:url" content="https://specs.namada.net" />
+    </>
+  );
+};
